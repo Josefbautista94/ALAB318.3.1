63c2aca8be497cc2da20a4e790729239cc3cc7b0
Jose <josefbautista94@gmail.com>
2025-04-09 23:33:22 -0400

    Add DELETE /api/comments/:id route to remove comment by ID

--- routes/comments.js
+++ routes/comments.js
@@ -60,16 +60,31 @@ router.get("/:id", (req, res, next) => { // setting up a get route that listens 
 })
 
 //PATCH /api/comments/:id
-router.patch("/:id", (req,res,next)=>{ // defining a PATCH route at /api/comments/:id
+router.patch("/:id", (req, res, next) => { // defining a PATCH route at /api/comments/:id
 
-    const comment = comments.find( c => c.id == req.params.id); //  Searches the comments array for a comment with a matching id
+    const comment = comments.find(c => c.id == req.params.id); //  Searches the comments array for a comment with a matching id
 
-    if(!comment){ // if no comment was found it would throw a 404
-        return next(error(404,"The comment wasn't found!"))
+    if (!comment) { // if no comment was found it would throw a 404
+        return next(error(404, "The comment wasn't found!"))
     }
 
-    comment.body =req.body.body || comment.body; // This updates the body of the comment ONLY if a new one is provided in the request, so if req.body.body exists, it replaces the old body, if not the comment stays the same(fallback assignment)
+    comment.body = req.body.body || comment.body; // This updates the body of the comment ONLY if a new one is provided in the request, so if req.body.body exists, it replaces the old body, if not the comment stays the same(fallback assignment)
     res.json(comment); //Responds with the updated comment as JSON but if there weren't any changes , made it returns the original
+})
+
+// DELETE /api/comments/:id
+router.delete("/:id", (req, res, next) => { // defining the a DELETE route at /api/comments/:id
+
+    const index = comments.findIndex(c => c.id == req.params.id); // Searching for the index of the comment in the comments array
+    //.findIndex() returns the index of the first match or -1 if no match is found
+
+    if (index === -1) { // If the comment doesn’t exist ( index is -1), it triggers your custom 404 error
+        return next(error(404, "The comment wasn't found!"))
+    }
+
+    const deleted = comments.splice(index, 1) // .splice() removes 1 item from the array at the found index. This permanently removes the comment from your in-memory comments array
+    res.json(deleted[0]) //  Responds with the deleted comment as JSON so the client can confirm what was removed
+    // Even though .splice() returns an array, we only want to return the single object removed
 })
 
 module.exports = router;
